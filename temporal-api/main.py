4aa551ef3ba219c2d194ff97c269bb6434c015c8
Sagar Shah <sagar.shah@simformsolutions.com>
2025-04-25 14:10:23 +0530

    reset_type in API param

--- temporal-api/main.py
+++ temporal-api/main.py
@@ -4,12 +4,12 @@ import subprocess
 app = FastAPI()
 
 @app.get("/reset-workflow/")
-def reset_workflow(workflow_id: str, run_id: str):
+def reset_workflow(workflow_id: str, run_id: str, reset_type: str = "FirstWorkflowTask"):
     cmd = [
         "tctl", "workflow", "reset",
         "--workflow_id", workflow_id,
         "--run_id", run_id,
-        "--reset_type", "FirstWorkflowTask",
+        "--reset_type", reset_type,
         "--reason", "reset from API"
     ]
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
